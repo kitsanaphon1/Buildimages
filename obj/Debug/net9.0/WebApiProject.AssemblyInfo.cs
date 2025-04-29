a3934de05675b55650f5c712795ba6b05074e2a4
kitsanaphon1 <123629506+kitsanaphon1@users.noreply.github.com>
2025-04-29 16:01:27 +0700

    update

--- obj/Debug/net9.0/WebApiProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApiProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21233cfd5366a24a2083f679438c73735dea3724")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf09e00d66228908bf4a93d55a1448d3804e1c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
